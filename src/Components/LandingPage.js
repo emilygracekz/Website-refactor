06f9663733461de3cf9383ad8e46356f05742733
emilygracekz <57361805+emilygracekz@users.noreply.github.com>
2020-04-24 07:54:35 -0500

    landing page styling and component

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -2,19 +2,13 @@ import React from "react";
 
 function LandingPage() {
   return (
-    <div class="container">
-      <div id="top" class="bg">
+    <div className="container">
+      <div className="bg">
         <h1 data-type>emily kondziola</h1>
         <h2 data-type>front-end developer</h2>
           <p>
-          Hi, my name is Emily and I'm a front-end web developer specializing in
-          <strong>Vanilla Javascript</strong>, 
-          <strong>React</strong> and
-          <strong>React Native</strong>. 
-          Please check out some of my projects
-          below, or view my
-          <a href="https://github.com/emilygracekz/Personal-website/raw/master/Emily_Kondziola_Resume_SP2020.pdf">
-          resume.
+          Hi, my name is Emily and I'm a front-end web developer specializing in <mark>Vanilla Javascript, React and React Native.</mark> Please check out some of my projects below, or view my 
+           <a href="https://github.com/emilygracekz/Personal-website/raw/master/Emily_Kondziola_Resume_SP2020.pdf"> resume.
           </a>
           <br />
           <br />
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-import './LandingPage';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
